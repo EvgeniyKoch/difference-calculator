75559c33d048407c4581999bbf264fe4eb83cb9b
ekochetov <ekochetov@roox.ru>
2020-03-28 09:59:28 +0300

    change the name of the variables

--- src/index.js
+++ src/index.js
@@ -7,14 +7,14 @@ import parse from './parser';
 import render from './formatters';
 
 export default (pathToFile1, pathToFile2, format) => {
-  const ext1 = path.extname(pathToFile1).slice(1);
-  const ext2 = path.extname(pathToFile2).slice(1);
+  const typeData1 = path.extname(pathToFile1).slice(1);
+  const typeData2 = path.extname(pathToFile2).slice(1);
 
   const fileDataBefore = fs.readFileSync(pathToFile1, 'utf8')
-    |> ((_) => parse(_, ext1));
+    |> ((_) => parse(_, typeData1));
 
   const fileDataAfter = fs.readFileSync(pathToFile2, 'utf8')
-    |> ((_) => parse(_, ext2));
+    |> ((_) => parse(_, typeData2));
 
   if (!fileDataBefore || !fileDataAfter) {
     throw new Error('There is nothing compare!');
